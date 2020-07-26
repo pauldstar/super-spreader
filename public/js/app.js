ff78c7c324c4c75b49cbffe95ff3296277edf0e6
ClassyD <olorunfemi.olaniyi@sky.uk>
2020-07-26 22:43:17 +0100

    added user information object removed todo

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,19 @@
 function app() {
     return {
         start: false,
+        questions: null,
+        leaderboard: null,
+        userInformation: {
+            username: '',
+            elapsedTime: '',
+            correctAnswers: 0
+        },
+        async mounted() {
+            console.log(this.questions);
+            const data = await Promise.all([getLeaderboard(), getQuestions()]);
+            this.leaderboard = data[0];
+            this.questions = data[1];
+        },
 
         startGame() {
             this.start = true;
--- public/js/firebaseService.js
+++ public/js/firebaseService.js
@@ -1,22 +1,27 @@
 function getQuestions() {
-    let questionsRef = firebase.database().ref('questions/');
-    questionsRef.on('child_added', function (data) {
-        console.log(data.val());
-    }, (error) => {
-        console.log(error);
-    });
+    return new Promise((resolve, reject) => {
+        let questionsRef = firebase.database().ref('questions/');
+        questionsRef.once('value', function (data) {
+            resolve(data.val())
+        }, (error) => {
+            console.log(error);
+            reject(error);
+        });
+    })
 }
 
 function getLeaderboard() {
-    let leaderBoardRef = firebase.database().ref('leaderboard/');
-    leaderBoardRef.on('child_added', function (data) {
-        console.log(data.val());
-    }, (error) => {
-        console.log(error);
-    });
+    return new Promise((resolve, reject) => {
+        let leaderBoardRef = firebase.database().ref('leaderboard/');
+        leaderBoardRef.once('value', function (data) {
+            resolve(data.val())
+        }, (error) => {
+            console.log(error);
+            reject(error)
+        });
+    })
 }
 
-//TODO - test to is if it works.
 function pushToLeaderboard(user) {
     var newPostKey = firebase.database().ref().child('questions').push().key;
     let leaderBoardRef = firebase.database().ref(`leaderboard/${newPostKey}`);
